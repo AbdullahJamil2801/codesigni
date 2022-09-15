0d96ae220427fe045f8827d8f759268064a3d4bc
AbdullahJamil28 <abdullahbinjamil028@gmail.com>
2022-09-15 05:50:00 -0700

    hero changes-2

--- src/components/HomeTwo/HeroSlider.jsx
+++ src/components/HomeTwo/HeroSlider.jsx
@@ -1,72 +1,82 @@
 import Link from 'next/link';
+import { Pagination, EffectFade } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, EffectFade, Autoplay } from "swiper";
 
-const HeroSlider = () => {
+
+const HeroSliderFour = () => {
   const sliderData = [
     {
       id: 1,
-      title: <>Introduce Our Creative <br /> Digital Agency.</>
+      bg: 'assets/img/slider/slider-2-1.jpg',
+      title: 'WE PROVIDE BEST SERVICE WITH EXCLUSIVE FEATURES',
+      subtitle: <>Business <span>Growth</span></>
     },
     {
       id: 2,
-      title: <>Testing the Changes <br /> Digital Agency.</>
+      bg: 'assets/img/slider/slider-2-1.jpg',
+      title: 'WE PROVIDE BEST SERVICE WITH EXCLUSIVE METARIALS',
+      subtitle: <>Business <span>Health</span></>
     },
     {
       id: 3,
-      title: <>Introduce Our Creative <br /> Digital Agency.</>
-    }
+      bg: 'assets/img/slider/slider-2-1.jpg',
+      title: 'WE PROVIDE BEST SERVICE WITH EXCLUSIVE SUPPORT',
+      subtitle: <>Business <span>Strong</span></>
+    },
   ]
   return (
     <>
-      <section className="tpagency-area p-relative box-plr-85">
-        <div className="container-fluid">
-          <div className="row align-items-center">
-            <div className="col-xl-12">
-              <div className="tpagency__slider tp-agency-slide-active p-relative">
+      <div className="tpbs-area p-relative">
+        <div className="tpbs-slider tpbs-slider-active">
 
-                <Swiper navigation={{ nextEl: '.tp-swiper-next', prevEl: '.tp-swiper-prev', }}
-                  modules={[Navigation, EffectFade, Autoplay]}
-                  spaceBetween={25}
-                  loop={true}
-                  effect="fade"
-                  slidesPerView={1}
-                  autoplay={{ delay: 6000 }}
-                >
-                  {sliderData.map(item => {
-                    return (
-                      <SwiperSlide key={item.id}>
-                        <div className="tpagency__slider-item tpagency__slider-height text-center d-flex align-items-center" style={{ backgroundImage: 'url(assets/img/slider/slider-img-1.jpg)', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
-                          <div className="tp-agency-slider-inner">
-                            <h2 className="tpagency-title">Introduce Our Creative <br /> Digital Agency.</h2>
-                            <div className="tpagency-button mt-60">
+          <Swiper
+            modules={[Pagination, EffectFade]}
+            spaceBetween={25}
+            slidesPerView={1}
+            effect="fade"
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 6000 }}
+          >
+            {sliderData.slice(0, 6).map(item => {
+              return (
+                <SwiperSlide key={item.id}>
+                  <div className="tpbs-slider__item tpbs-slider__height text-center" style={{ backgroundImage: `url(${item.bg})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
+                    <div className="dot-bg-iamge" data-background="assets/img/bg/dot-img.png"></div>
+                    <div className="tpbs-slider__item-content p-relative">
+                      <div className="container">
+                        <div className="row">
+                          <div className="col-xl-12">
+                            <span className="tp-sub-title-2 mb-30"><a>{item.title}</a></span>
+                            <h3 className="tpbs-slider__item-title">{item.subtitle}</h3>
+                            <div className="tpbs-btn mt-40">
                               <Link href="/services">
-                                <a className="tp-white-btn">See Nerox Project</a>
+                                <a className="tp-solid-btn">Free Consultation</a>
                               </Link>
                             </div>
                           </div>
                         </div>
-                      </SwiperSlide>
-                    )
-                  })
-                  }
-                </Swiper>
+                      </div>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              )
+            })
+            }
+          </Swiper>
 
-                <div>
-                  <button type="button" className="tp-swiper-prev swiper-arrow"><i className="far fa-long-arrow-left"></i></button>
-                  <button type="button" className="tp-swiper-next swiper-arrow"><i className="far fa-long-arrow-right"></i></button>
-                </div>
-
-              </div>
-              <div className="tpagency__down-btn">
-                <a href="#tpservices-area"><i className="far fa-long-arrow-down"></i></a>
-              </div>
-            </div>
-          </div>
         </div>
-      </section>
+        <div className="tpbs__shape tpbs__shape-1">
+          <img src="assets/img/shape/shape-1.png" alt="shape-1" />
+        </div>
+        <div className="tpbs__shape tpbs__shape-2">
+          <img src="assets/img/shape/shape-2.png" alt="shape-1" />
+        </div>
+        <div className="tpbs__shape tpbs__shape-3">
+          <img src="assets/img/shape/shape-3.png" alt="shape-1" />
+        </div>
+      </div>
     </>
   );
 };
 
-export default HeroSlider;
+export default HeroSliderFour;
